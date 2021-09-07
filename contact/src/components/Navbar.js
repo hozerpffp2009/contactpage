764ba592c78b2125196bd939fef5ded58d06a21b
hozerpffp <hozerpffp2009@gmail.com>
2021-09-06 20:58:00 -0500

    added image to home page. added drop down menu.

--- contact/src/components/Navbar.js
+++ contact/src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import Nav from 'react-bootstrap/Nav'
 
 class Navbar extends Component {
     state = {
@@ -8,17 +7,28 @@ class Navbar extends Component {
 
     render() {
         return(
-            <Nav className="justify-content-center" activeKey="/home">
+          <nav>
+            <div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="/">Home</a>
+    <a href="contact">Contact</a>
+    <a href="portfolio">Portfolio</a>
+  </div>
+</div>
+            {/* <Nav className="justify-content-center" activeKey="/home">
             <Nav.Item>
-              <Nav.Link href="/">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
+               <Nav.Link href="/">Home</Nav.Link>
+           </Nav.Item>
+             <Nav.Item>
               <Nav.Link href="contact">Contact</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+             <Nav.Item>
               <Nav.Link href="portfolio">Portfolio</Nav.Link>
             </Nav.Item>
-          </Nav>
+           </Nav> */}
+          </nav>
+     
         );
     };
 }
--- contact/src/pages/Home.js
+++ contact/src/pages/Home.js
@@ -1,56 +1,23 @@
 import React, {Component} from "react";
-import Card from 'react-bootstrap/Card'
-import Accordion from 'react-bootstrap/Accordion'
+import joseph from "../images/joseph.jpg"
 
 class Home extends Component {
 
 render() {
     return (
-        <body>
-            <div class="container">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-            <Card.Title>About Me</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-        </Card.Body>
-        </Card>
+      <body>
+        <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+         <img src={joseph} id="josephimg" alt="joseph img"/>
+          </div>
+          <div class="flip-card-back">
+            <h1>Joseph Arocha</h1>
+            <p>Full Stack Web Developer and Licensed Plumber</p>
+          </div>
         </div>
-
-        <div>
-        <Accordion>
-  <Accordion.Item eventKey="0">
-    <Accordion.Header>Accordion Item #1</Accordion.Header>
-    <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Accordion.Body>
-  </Accordion.Item>
-  <Accordion.Item eventKey="1">
-    <Accordion.Header>Accordion Item #2</Accordion.Header>
-    <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-        </div>
-        </body>
-        
-        
+      </div>
+    </body>
      );
     }
 }
